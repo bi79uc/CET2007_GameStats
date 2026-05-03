d2a5fb302bef972ad9aaf8bbbd4bb197362a61a5
Matthew Gooch <bi79uc@student.sunderland.ac.uk>
2026-05-03 14:25:47 +0100

    Fix player model and repository namespaces

--- Data/IPlayerRepository.cs
+++ Data/IPlayerRepository.cs
@@ -1,27 +1,15 @@
 ﻿using CET2007_GameStats.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
-namespace CET2007_GameStats.Data
-{
-    interface IPlayerRepository
-    {
-        // get all players
+namespace CET2007_GameStats.Data {
+    public interface IPlayerRepository {
         List<Player> GetAll();
-        // get player by id
         Player? GetById(int id);
-        // get player by username
         Player? GetByUsername(string username);
-        
         void Add(Player player);
-        // update player's stats
-        bool Update(Player player);
-        // remove player by id
-        bool Remove(int id);
-        
+        void Update(Player player);
+        void Remove(int id);
         void Load();
         void Save();
     }
--- Models/Player.cs
+++ Models/Player.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CET2007_GameStats.Models
-{
-    class Player
-    {
+﻿namespace CET2007_GameStats.Models {
+    public class Player {
         public int Id { get; set; }
         public string Username { get; set; } = "";
         public double HoursPlayed { get; set; }
